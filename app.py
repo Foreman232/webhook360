aa29a8d696a87aa4bc5e615b014c2246cb87cf0a
Foreman232 <axel.sambrano@bpoinnovations.com>
2025-05-23 11:50:43 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 mensajes = []
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def webhook():
-    if request.method == 'GET':
-        return 'Verificación exitosa', 200
-    elif request.method == 'POST':
+    if request.method == "GET":
+        return "Verificación exitosa", 200
+    elif request.method == "POST":
         data = request.get_json()
         mensajes.append(data)
         print("📩 Mensaje recibido:", data)
-        return 'ok', 200
-    else:
-        return '', 200
+        return "ok", 200
+
+@app.route("/chat", methods=["GET"])
+def chat():
+    return render_template("chat.html")
