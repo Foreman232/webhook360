f8b4265757b6501a75d788fd11bdbb4f589093fa
Foreman232 <axel.sambrano@bpoinnovations.com>
2025-05-22 22:42:39 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,21 +3,12 @@ from flask import Flask, request
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
-def verify():
-    mode = request.args.get("hub.mode")
-    challenge = request.args.get("hub.challenge")
-    token = request.args.get("hub.verify_token")
+def home():
+    return "¡Servidor activo correctamente!"
 
-    if mode == "subscribe" and token == "webhook123":
-        return challenge, 200
-    else:
-        return "Error de verificación", 403
-
-@app.route("/", methods=["POST"])
-def receive():
+@app.route("/webhook", methods=["POST"])
+def webhook():
     data = request.get_json()
     print("Mensaje recibido:", data)
-    return "ok", 200
+    return "Recibido", 200
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
