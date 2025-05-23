598873ab638ba7ad0ad7695c49b3ab4775b32ab4
Foreman232 <axel.sambrano@bpoinnovations.com>
2025-05-23 10:29:24 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,23 +1,24 @@
+
 from flask import Flask, request, render_template, redirect
 import requests
 
 app = Flask(__name__)
 mensajes = []
 
-@app.route("/", methods=["GET", "POST"])
+@app.route('/', methods=['GET', 'POST'])
 def webhook():
-    if request.method == "GET":
-        return "Verificación exitosa", 200
-    elif request.method == "POST":
+    if request.method == 'GET':
+        return 'Verificación exitosa', 200
+    elif request.method == 'POST':
         data = request.get_json()
         mensajes.append(data)
-        return "ok", 200
+        return 'ok', 200
 
-@app.route("/chat", methods=["GET", "POST"])
+@app.route('/chat', methods=['GET', 'POST'])
 def chat():
-    if request.method == "POST":
-        telefono = request.form["numero"]
-        texto = request.form["mensaje"]
+    if request.method == 'POST':
+        telefono = request.form['numero']
+        texto = request.form['mensaje']
         payload = {
             "recipient_type": "individual",
             "to": telefono,
@@ -25,8 +26,8 @@ def chat():
             "text": {"body": texto}
         }
         headers = {
-            "D360-API-KEY": "yxKGn4IO24k4MRONILaJxG7xAK"
+            "D360-API-KEY": "REEMPLAZA_TU_API_KEY"
         }
         requests.post("https://waba-v2.360dialog.io/messages", json=payload, headers=headers)
-        return redirect("/chat")
-    return render_template("chat.html", mensajes=mensajes)
+        return redirect('/chat')
+    return render_template('chat.html', mensajes=mensajes)
