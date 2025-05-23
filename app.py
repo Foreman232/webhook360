f47d1d78aa3f09f1fe8d26319c29743f4f579a14
Foreman232 <axel.sambrano@bpoinnovations.com>
2025-05-23 10:58:24 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,16 @@
 from flask import Flask, request, render_template, redirect
-import requests
 
 app = Flask(__name__)
 mensajes = []
 
-@app.route("/", methods=["GET", "POST"])
+@app.route('/', methods=['GET', 'POST'])
 def webhook():
-    if request.method == "GET":
-        return "Verificación exitosa", 200
-    elif request.method == "POST":
+    if request.method == 'GET':
+        return 'Verificación exitosa', 200
+    elif request.method == 'POST':
         data = request.get_json()
         mensajes.append(data)
         print("📩 Mensaje recibido:", data)
-        return "ok", 200
-
-@app.route("/chat", methods=["GET", "POST"])
-def chat():
-    if request.method == "POST":
-        telefono = request.form["numero"]
-        texto = request.form["mensaje"]
-        payload = {
-            "recipient_type": "individual",
-            "to": telefono,
-            "type": "text",
-            "text": {"body": texto}
-        }
-        headers = {
-            "D360-API-KEY": "yxKGn4IO24k4MRONILaJxG7xAK"
-        }
-        requests.post("https://waba-v2.360dialog.io/messages", json=payload, headers=headers)
-        return redirect("/chat")
-    return render_template("chat.html", mensajes=mensajes)
+        return 'ok', 200
+    else:
+        return '', 200
