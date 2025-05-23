505d415b834f68de7142b6f1f04cdd964ea9ae66
Foreman232 <axel.sambrano@bpoinnovations.com>
2025-05-23 08:56:39 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,41 +1,37 @@
 
-from flask import Flask, render_template, request, redirect
-from flask_socketio import SocketIO, emit
+from flask import Flask, request, render_template, redirect
 import requests
 
 app = Flask(__name__)
-socketio = SocketIO(app)
 mensajes = []
 
-@app.route("/", methods=["GET"])
-def index():
-    return render_template("index.html", mensajes=mensajes)
-
-@app.route("/", methods=["POST"])
-def send_message():
-    numero = request.form["numero"]
-    mensaje = request.form["mensaje"]
-    payload = {
-        "recipient_type": "individual",
-        "to": numero,
-        "type": "text",
-        "text": {"body": mensaje}
-    }
-    headers = {
-        "D360-API-KEY": "yxKGn4IO24k4MRONILaJxG7xAK"
-    }
-    requests.post("https://waba-v2.360dialog.io/messages", json=payload, headers=headers)
-    return redirect("/")
-
-@app.route("/webhook", methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
 def webhook():
     if request.method == "GET":
         return "Verificación exitosa", 200
     elif request.method == "POST":
         data = request.get_json()
         mensajes.append(data)
-        socketio.emit("nuevo_mensaje", data)
+        print("📩 Mensaje recibido:", data)
         return "ok", 200
 
+@app.route("/chat", methods=["GET", "POST"])
+def chat():
+    if request.method == "POST":
+        telefono = request.form["numero"]
+        texto = request.form["mensaje"]
+        payload = {
+            "recipient_type": "individual",
+            "to": telefono,
+            "type": "text",
+            "text": {"body": texto}
+        }
+        headers = {
+            "D360-API-KEY": "yxKGn4IO24k4MRONILaJxG7xAK"
+        }
+        requests.post("https://waba-v2.360dialog.io/messages", json=payload, headers=headers)
+        return redirect("/chat")
+    return render_template("chat.html", mensajes=mensajes)
+
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000)
+    app.run()
